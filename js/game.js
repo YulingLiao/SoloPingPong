3f555c4baeff1111e68e517f675a7cefea086068
YulingLiao <yl839@drexel.edu>
2016-06-28 14:48:37 -0400

    Week 02 changes
    Starting to build solo pingpong game

--- js/game.js
+++ js/game.js
@@ -19,4 +19,38 @@ window.cancelRequestAnimFrame = ( function() {
 		clearTimeout;
 } )();
 
-console.log('Holla');
+// DO NOT TOUCH THE CODE ABOVE
+
+//console.log('Holla');
+
+//Step 01 ..yl.. Create game canvas and track mouse position
+    var gameCanvas = document.getElementById("canvas");
+    //Store HTML5 canvas tag into JS variable
+    
+    var ctx = gameCanvas.getContext("2d"); // Create context 2D
+    var W = window.innerWidth;
+    var H = window.innerHeight;
+
+    var mouseObj = {};
+
+//    console.log('broser width is currenly' + W);
+//    console.log('broser width is currenly' + H);
+    
+    gameCanvas.width = W;
+    gameCanvas.height = H;
+
+    function paintCanvas(){
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, W, H);
+    }
+    paintCanvas();//call function
+
+
+
+
+    function trackPosition(evt){
+        mouseObj.x = evt.pageX;
+        mouseObj.y = evt.pageY;
+        console.log("cursor x is : " + mouseObj.x + " cursor y is : " + mouseObj.y);
+    }
+    gameCanvas.addEventListener("mousemove", trackPosition, true);
